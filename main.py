133570cde4e860939c1ba40b35228a3ee91766bf
shcheglusha1105 <59973196+shcheglusha1105@users.noreply.github.com>
2023-07-21 14:40:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+myName = input("What's you name?: ")
+myAge = input("How old are you? :")
+myAge = int(myAge)
+if myAge >= 30:
+    print("oh my god life is over")
+else:
+    print("you are so young i envy you!")
